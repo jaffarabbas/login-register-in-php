c96fb9563d85040281d44c20dafe80f06ff9ea6c
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-30 17:22:05 +0500

    30/05/2022 17:22:05

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include("header.php") ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body text-center">
+                    <a href="login.php" class="btn btn-primary">LOGIN</a><br>
+                    <a href="register.php" class="btn btn-primary mt-2">REGISTER</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include("footer.php") ?>
